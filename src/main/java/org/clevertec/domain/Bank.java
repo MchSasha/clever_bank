dea001fcce9b58e9e0806d047f0a1ab3e2203686
Александрия <@DESKTOP-H224QSD>
2023-08-23 11:57:53 +0300

    Add Bank domain

---
+++ src/main/java/org/clevertec/domain/Bank.java
@@ -0,0 +1,11 @@
+package org.clevertec.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Bank {
+    private int bankId;
+    private String name;
+}
