20687f8992d86b6a896b88de46ecf459eb174dfd
Александрия <@DESKTOP-H224QSD>
2023-08-25 01:47:59 +0300

    Add to AccountService transaction info interface

--- src/main/java/org/clevertec/services/AccountService.java
+++ src/main/java/org/clevertec/services/AccountService.java
@@ -3,6 +3,9 @@ package org.clevertec.services;
 import org.clevertec.domain.Account;
 import org.clevertec.domain.Transaction;
 
+import java.sql.Date;
+import java.util.List;
+
 public interface AccountService {
     Transaction withdrawMoney(String accountNumber, Integer sum);
 
@@ -11,4 +14,11 @@ public interface AccountService {
     Transaction transferMoney(String senderAccountNumber, String recipientAccountNumber, Integer sum);
 
     Account getAccount(String accountNumber);
+
+    List<Transaction> getAllTransactions(String accountNumber, Date from, Date to);
+
+    int getAllIncome(String accountNumber, Date from, Date to);
+
+    int getAllWithdrawal(String accountNumber, Date from, Date to);
+
 }
