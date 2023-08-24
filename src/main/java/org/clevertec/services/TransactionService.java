6da0dd3d51c5b9099fd4ed5dc02e2f3d67369f6b
Александрия <@DESKTOP-H224QSD>
2023-08-25 01:48:44 +0300

    Add TransactionService

---
+++ src/main/java/org/clevertec/services/TransactionService.java
@@ -0,0 +1,10 @@
+package org.clevertec.services;
+
+import org.clevertec.domain.Transaction;
+
+
+public interface TransactionService {
+    String getComment(Transaction transaction);
+
+    String getExtendedComment(Transaction transaction);
+}
