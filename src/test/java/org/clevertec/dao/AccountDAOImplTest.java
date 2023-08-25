01514735e83b2423945145d903d2f570c212297b
Александрия <@DESKTOP-H224QSD>
2023-08-26 01:57:40 +0300

    Add AccountDAOImplTest.java, Add updateBalance, getAccountId, getTotalIncome tests

---
+++ src/test/java/org/clevertec/dao/AccountDAOImplTest.java
@@ -0,0 +1,78 @@
+package org.clevertec.dao;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AccountDAOImplTest {
+
+    @Test
+    void updateBalance() {
+        AccountDAO accountDAO = new AccountDAOImpl();
+        int affRows;
+        try {
+            affRows = accountDAO.updateBalance(5, 1000);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(1, affRows);
+    }
+    @Test
+    void updateBalanceUnsuccessful() {
+        AccountDAO accountDAO = new AccountDAOImpl();
+        int affRows;
+        try {
+            affRows = accountDAO.updateBalance(1000, -1000);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(0, affRows);
+    }
+
+    @Test
+    void getAccountId() {
+        AccountDAO accountDAO = new AccountDAOImpl();
+        int id;
+        try {
+            id = accountDAO.getAccountId( "AB1234567890123456789012AAAA");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(1, id);
+    }
+    @Test
+    void getAccountIdUnsuccessful() {
+        AccountDAO accountDAO = new AccountDAOImpl();
+        int id;
+        try {
+            id = accountDAO.getAccountId( "1234567890123456789012AAAA");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(-1, id);
+    }
+
+    @Test
+    void getTotalIncome() {
+        AccountDAO accountDAO = new AccountDAOImpl();
+        int income;
+        try {
+            income = accountDAO.getTotalIncome(
+                    6,
+                    Date.valueOf(LocalDate.now().minusDays(2)),
+                    Date.valueOf(LocalDate.now()));
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(1415.00, income);
+    }
+}
