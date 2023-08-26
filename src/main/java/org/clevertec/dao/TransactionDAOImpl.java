ee22b386164122eeb8ccb67bb74ac10c3811b8a6
Александрия <@DESKTOP-H224QSD>
2023-08-26 21:11:35 +0300

    Add saveTransaction to TransactionDAOImpl

---
+++ src/main/java/org/clevertec/dao/TransactionDAOImpl.java
@@ -0,0 +1,41 @@
+package org.clevertec.dao;
+
+import org.clevertec.domain.Transaction;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TransactionDAOImpl implements TransactionDAO{
+    public static final String SQL_INSERT_TRANSACTION =
+            "INSERT INTO Transactions(SenderAccountId, RecipientAccountId, Date, Sum) " +
+            "VALUES (?, ?, NOW(), ?);";
+
+    @Override
+    public void saveTransaction(Transaction transaction) {
+        try(Connection connection = DatabaseUtility.getConnection();
+            PreparedStatement statement = connection.prepareStatement(SQL_INSERT_TRANSACTION)) {
+
+            statement.setInt(1, transaction.getSenderAccountId());
+            statement.setInt(2, transaction.getRecipientAccountId());
+            statement.setDouble(3, transaction.getSum());
+
+            statement.executeUpdate();                      //??
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Transaction> getInitiatedTransactions(String accountNumber, Date from, Date to) {
+        return null;
+    }
+
+    @Override
+    public List<Transaction> getNotInitiatedTransactions(String accountNumber, Date from, Date to) {
+        return null;
+    }
+}
