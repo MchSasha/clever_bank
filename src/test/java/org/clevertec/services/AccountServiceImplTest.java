e1d48a3487bc8bbd4271aa271135f152a0f550cc
Александрия <@DESKTOP-H224QSD>
2023-08-28 23:27:29 +0300

    Add getTotalIncome(), getTotalWithdrawal(), getAccountId() tests, refactor withdrawMoney, putMoney names  in AccountServiceImplTest

--- src/test/java/org/clevertec/services/AccountServiceImplTest.java
+++ src/test/java/org/clevertec/services/AccountServiceImplTest.java
@@ -2,6 +2,7 @@ package org.clevertec.services;
 
 import org.clevertec.domain.Transaction;
 import org.clevertec.exceptions.AccountNotFoundException;
+import org.clevertec.exceptions.InsufficientFundsException;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Date;
@@ -16,19 +17,18 @@ class AccountServiceImplTest {
     void withdrawMoney() {////no acc exc
         Transaction transaction = new AccountServiceImpl().withdrawMoney("MN1234567890123456789012AAAA", 8);
 
-        assertEquals(new Transaction(null,7, null, Date.valueOf(LocalDate.now()), -8.0), transaction);
+        assertEquals(new Transaction(null,7, null, Date.valueOf(LocalDate.now()), -8.0),
+                transaction);
     }
     @Test
     void withdrawMoneyWithNoSuchAccount() {////?????????? -1 id no acc exc
-        assertThrows(AccountNotFoundException.class, () -> {
-            Transaction transaction = new AccountServiceImpl().withdrawMoney("MN1234567893456789012AAAA", 8);
-        });
+        assertThrows(AccountNotFoundException.class,
+                () -> new AccountServiceImpl().withdrawMoney("MN1234567893456789012AAAA", 8));
     }
     @Test
     void withdrawMoneyInsufficientSum() {////?????????? id found but 0 rows inserted
-        assertThrows(InsufficientFundsException.class, () -> {
-            Transaction transaction = new AccountServiceImpl().withdrawMoney("MN1234567890123456789012AAAA", 100000);
-        });
+        assertThrows(InsufficientFundsException.class,
+                () -> new AccountServiceImpl().withdrawMoney("MN1234567890123456789012AAAA", 1000000));
     }
 
 
@@ -36,13 +36,13 @@ class AccountServiceImplTest {
     void putMoney() {
         Transaction transaction = new AccountServiceImpl().putMoney("MN1234567890123456789012AAAA", 200000);
 
-        assertEquals(new Transaction(null,7, null, Date.valueOf(LocalDate.now()), 200000.0), transaction);
+        assertEquals(new Transaction(null,7, null, Date.valueOf(LocalDate.now()), 200000.0),
+                transaction);
     }
     @Test
     void putMoneyWithNoSuchAccount() {
-        assertThrows(AccountNotFoundException.class, () -> {
-            Transaction transaction = new AccountServiceImpl().putMoney("MN1234567893456789012AAAA", 8);
-        });
+        assertThrows(AccountNotFoundException.class,
+                () -> new AccountServiceImpl().putMoney("MN1234567893456789012AAAA", 8));
     }
 
     @Test
@@ -58,14 +58,30 @@ class AccountServiceImplTest {
     }
 
     @Test
-    void getAllIncome() {
+    void getTotalIncome() {//2
+        assertEquals(12500.0,
+                new AccountServiceImpl().getTotalIncome("CD1234567890123456789012AAAA",
+                        Date.valueOf(LocalDate.now().minusYears(1)),
+                        Date.valueOf(LocalDate.now())));
+
     }
 
     @Test
-    void getAllWithdrawal() {
+    void getTotalWithdrawal() {
+        assertEquals(-11700.0,
+                new AccountServiceImpl().getTotalWithdrawal("CD1234567890123456789012AAAA",
+                        Date.valueOf(LocalDate.now().minusYears(1)),
+                        Date.valueOf(LocalDate.now())));
     }
 
     @Test
     void getAccountId() {
+        assertEquals(2,
+                new AccountServiceImpl().getAccountId("CD1234567890123456789012AAAA"));
+    }
+    @Test
+    void getAccountIdWithNoSuchAccount() {
+        assertEquals(-1,
+                new AccountServiceImpl().getAccountId("CD12345690123456789012AAAA"));
     }
 }
