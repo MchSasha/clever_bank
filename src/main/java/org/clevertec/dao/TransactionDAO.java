2efa5a4b274fa91eccfe62a1efbad320ffb5d060
Александрия <@DESKTOP-H224QSD>
2023-08-28 16:48:07 +0300

    Refactor TransactionDAO interface

--- src/main/java/org/clevertec/dao/TransactionDAO.java
+++ src/main/java/org/clevertec/dao/TransactionDAO.java
@@ -2,15 +2,9 @@ package org.clevertec.dao;
 
 import org.clevertec.domain.Transaction;
 
-import java.sql.Date;
-import java.util.List;
-
 public interface TransactionDAO {
     int saveTransaction(Transaction transaction);
 
-    List<Transaction> getInitiatedTransactions(String accountNumber, Date from, Date to);
-
-    List<Transaction> getNotInitiatedTransactions(String accountNumber, Date from, Date to);
 }
 
 /*
